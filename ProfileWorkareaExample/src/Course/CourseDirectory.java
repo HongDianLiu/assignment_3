da6f054ac1ebcec850e04da06b6b545e7514e7b7
red_spot <liu.hongd@northeastern.edu>
2023-10-20 13:54:04 -0400

    234

---
+++ ProfileWorkareaExample/src/Course/CourseDirectory.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Course;
+
+/**
+ *
+ * @author MIS
+ */
+public class CourseDirectory {
+    private String pName;
+    private String topic;
+    private String region;
+    private String language;
+    private Arraylist<String> studentList;
+    
+}

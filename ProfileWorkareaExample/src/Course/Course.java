7373b45ac1648aa12bcfc404fee8c40d343a9efa
red_spot <liu.hongd@northeastern.edu>
2023-10-21 14:58:47 -0400

    coursedata

--- ProfileWorkareaExample/src/Course/Course.java
+++ ProfileWorkareaExample/src/Course/Course.java
@@ -4,10 +4,57 @@
  */
 package Course;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author MIS
  */
 public class Course {
+    private String pName;
+    private String topic;
+    private String region;
+    private String language;
+    private ArrayList<String> studentList;
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public ArrayList<String> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(ArrayList<String> studentList) {
+        this.studentList = studentList;
+    }
     
 }
